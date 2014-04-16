b67f73690f5a20cba1ccf749ca71ca9b854a6d41
twstddev <twstd.dev@gmail.com>
2014-04-16 15:56:00 +0000

    Add sortable index page

--- spec/dummy/app/admin/page.rb
+++ spec/dummy/app/admin/page.rb
@@ -1,4 +1,11 @@
 ActiveAdmin.register Page do
+	filter :title
+
+	config.sort_order = "position_asc"
+	config.paginate = false
+
+	sortable
+	
 	controller do
 		def permitted_params
 			params.permit!
@@ -7,6 +14,24 @@ ActiveAdmin.register Page do
 		def find_resource
 			scoped_collection.friendly.find( params[ :id ] )
 		end
+
+		def update
+			update! do |format|
+				format.html { redirect_to_page_editing }
+			end
+		end
+
+		def create
+			create! do |format|
+				format.html { redirect_to_page_editing }
+			end
+		end
+
+		private
+
+			def redirect_to_page_editing
+				redirect_to edit_admin_page_url( @page.friendly_id )
+			end
 	end
 
 	form do |f|
@@ -17,5 +42,14 @@ ActiveAdmin.register Page do
 		end
 
 		f.actions
+	end
+
+	index as: :sortable do
+		label "Title" do |page|
+			link_to page.title, edit_admin_page_path( page )
+		end
+		actions defaults: false do |page|
+			link_to "Delete", admin_page_path( page ), method: "delete", confirm: "Are you sure you want to delete this?" 
+		end
 	end
 end
